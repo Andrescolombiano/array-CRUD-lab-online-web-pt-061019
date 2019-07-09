d2c7c85c8361a90e9324b41c2912fdf2f29dcfa0
Andres Carvajal <jc1993box@hotmail.com>
2019-07-09 20:37:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,9 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  colors=["yellow","blue","red","white"]
-  colors.push("array")
-  puts colors.inspect
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
