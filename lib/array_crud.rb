5de3dea3391ec65725697367bfea364f336da870
Andres Carvajal <jc1993box@hotmail.com>
2019-07-09 20:13:20 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,17 +1,17 @@
 def create_an_empty_array
-  
+ [] 
 end
 
 def create_an_array
-  
+ colors ["yellow","blue","red","white"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  colors << "black"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  letters.unshift("black")
 end
 
 def remove_element_from_end_of_array(array)
